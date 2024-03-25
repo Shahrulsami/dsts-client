ec76afae65798ede94a6001d53515ac2b5f63dcc
mdsajedul <sajedul.islam@bizzntek.com>
2024-03-25 13:42:12 +0600

    added some file

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: 'dsts-client',
+    output: 'export',
+    reactStrictMode: true,
+  };
+
+  module.exports = nextConfig;
